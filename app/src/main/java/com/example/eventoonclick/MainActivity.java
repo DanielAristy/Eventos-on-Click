277863cc1ca754bb8b706c720d23bfdfcdec16f5
DanielAristy <danielariaty@gmail.com>
2020-02-18 23:51:53 -0500

    Se realizan eventos Click lambda, legacy y layaout

--- app/src/main/java/com/example/eventoonclick/MainActivity.java
+++ app/src/main/java/com/example/eventoonclick/MainActivity.java
@@ -11,10 +11,14 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     /*Declaracion de los componentes que se hacen
-    * en la vista*/
+     * en la vista*/
     private EditText txtNombre;
     private Button btnAceptar1;
     private Button btnAceptar2;
+
+    /*
+     * No es necesario declararlo
+     * ni instanciarlo*/
     private Button btnAceptar3;
 
     @Override
@@ -23,24 +27,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initComponent();
         onClickLegacy();
+        onClickLambda();
 
+    }
+
+    private void onClickLambda() {
+        btnAceptar2.setOnClickListener(v -> {
+            showToast();
+        });
     }
 
     private void onClickLegacy() {
         btnAceptar1.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                String nombre = txtNombre.getText().toString();
-                Toast.makeText(getApplicationContext(),nombre,Toast.LENGTH_SHORT).show();
-                txtNombre.setText("");
+                showToast();
             }
         });
     }
 
+    public void onclickFromLayout(View view) {
+        showToast();
+    }
+
+    private void showToast() {
+        String nombre = txtNombre.getText().toString();
+        Toast.makeText(getApplicationContext(), nombre, Toast.LENGTH_SHORT).show();
+        txtNombre.setText("");
+    }
+
     private void initComponent() {
+
         txtNombre = findViewById(R.id.txtNombre);
         btnAceptar1 = findViewById(R.id.btnAceptar1);
         btnAceptar2 = findViewById(R.id.btnAceptar2);
-        btnAceptar3 = findViewById(R.id.btnAceptar3);
     }
 }
